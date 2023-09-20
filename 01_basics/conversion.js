d1ebb7a94397712f7af9d31bf0c2d7d15f395f9e
Ankit-Asthana <ankitasthana486@gmail.com>
2023-09-20 15:14:23 +0530

    Conversion of datatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,28 @@
+let a="77abc";
+console.log(typeof a);
+
+let valueInNumber = Number(a);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"77" => 77
+// "77adh"=> NaN
+//true => 1; false => 0
+
+let isLoggedIn="";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true 
+// 0 => false
+// "siodhfd" => true
+// "" => false
+
+let num = 22;
+let stringNumber = String(num);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// 22 => "22"
+// number => string
